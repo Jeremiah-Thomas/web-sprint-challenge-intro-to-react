7961688cbb523668f1fc041b501fb8a67f6bb4a3
Jeremiah-Thomas <jeremiah.b.thomas12@gmail.com>
2022-06-17 02:08:56 -0400

    added cards for each character and used styled components for styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import Character from "./components/Character";
+
+const CharacterList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+  width: 90vw;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  const [data, setData] = useState();
+  const [isLoaded, setIsLoaded] = useState(false);
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((res) => {
+        setData(res.data);
+        setIsLoaded(true);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoaded(false);
+      });
+  }, []);
+  console.log(data);
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <>
+      {!isLoaded && <p>Loading...</p>}
+      {isLoaded && (
+        <div className="App">
+          {console.log(data)}
+          <h1 className="Header">Characters</h1>
+
+          <CharacterList>
+            {data.map((char) => {
+              return <Character key={char.name} data={char}></Character>;
+            })}
+          </CharacterList>
+        </div>
+      )}
+    </>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,21 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const CharCard = styled.div`
+  border: solid black 2px;
+`;
+
+const Character = (props) => {
+  return (
+    <CharCard>
+      <h1>{props.data.name}</h1>
+      <p>Gender: {props.data.gender}</p>
+      <p>Eye Color: {props.data.eye_color}</p>
+      <p>Height: {props.data.height}cm</p>
+      <p>Mass: {props.data.mass}kg</p>
+    </CharCard>
+  );
+};
+
+export default Character;
